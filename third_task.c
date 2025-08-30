0237503d6658c031644d6c690db435524f6d8a3b
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-30 11:03:53 +0200

    Update third_task.c

--- third_task.c
+++ third_task.c
@@ -1,56 +1,12 @@
 /* Task description:
-Check the HP for graphics. It is needed here!
-Look at the short code below with a few variables: a real and two pointers
-
-
-Figure out what the program prints without running it! At last run it to test your result.
-If what you expected does not align with the result, use the debugger! (line-by-line)
+Write a function that receives two numbers, and return !!both!! their sum and their product!
+one function has to do that!
 */
-
-
-void func1(int i2) {
-    i2 *= 2;
-    printf("func1()... i2 = %d\n", i2);     // 4
-}
-
-void func2(int *p3) {
-    *p3 *= 2;
-    printf("func2()... *p3 = %d\n", *p3);
-    p3 = NULL;                              // 5, 6
-    printf("func2()... p3 = %p\n", p3);
-}
 
 
 
 int main(void) {
-    int i1 = 2;
-    int *p1 = NULL;
-    int *p2 = NULL;
-    printf("Where do p1, p2 point to?\n");  // 1
-
-    p1 = &i1;
-    printf("&i1 = %p, p1 = %p\n", &i1, p1);
-    printf("i1 = %d, *p1 = %d\n", i1, *p1); // 2
-
-    i1 = 3;  printf("*p1 = %d\n", *p1);
-    *p1 = 4; printf("i1 = %d\n", i1);
-
-    p2 = p1;
-    *p2 = 5;
-    printf("i1 = %d, p2 = %p\n", i1, p2);   // 3
-    printf("-----\n");
-
-    func1(i1);
-    printf("main()... i1 = %d - but why?\n", i1);
-    printf("-----\n");
-
-    func2(&i1);
-    printf("main()... i1 = %d - why?\n", i1);
-    printf("-----\n");
-
-    func2(p2);
-    printf("main()... i1 = %d - why?\n", i1);
-    printf("main()... p2 = %p - why?\n", p2);
+   
 
     return 0;
 }
