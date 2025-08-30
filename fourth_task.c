c79a6008c13a7b1c8e4e9a6ebf93d7d500c50f7b
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-30 11:06:06 +0200

    Update fourth_task.c

--- fourth_task.c
+++ fourth_task.c
@@ -1,5 +1,11 @@
 /* Task description:
+1.  Create a function that receives an array of integers as parameter, and one integer value that has to be found in the array! 
+    Return the index of the element if the value was found, or return −1, if it was not. (If the value occurs multiple times in the array, it is up to you which index you return.)
+    Print the array with the indices to check the result of your function. Print the index returned by the function, or the text „not found”!
 
+2.  Modify the function (and the program) to return the memory address, instead of the index of the found element! 
+    What should be returned when the value was not found in the array? Using the modified function how can you determine the index of the found element (without another search)?
+    Modify the loops in the program to use pointers instead of indices. 
 
 */
 
