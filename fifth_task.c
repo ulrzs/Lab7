b9086f13e75faf3f06cb256780f27687c5f8e77f
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-30 11:06:34 +0200

    Update fifth_task.c

--- fifth_task.c
+++ fifth_task.c
@@ -1,5 +1,10 @@
 /* Task description:
+Write a function that receives an integer array, and separates the even and the odd elements of the array.
+The even elements are put into a second array, and the odd ones are put into a third array; both of them are function arguments, and are assumed to be large enough to store all even and odd elements.
 
+Write a function to print the integer array received as a function parameter!
+
+Write a main function that separates the even and odd elements of an array and prints them to the screen!
 
 */
 
