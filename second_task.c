db450b8d43328898766059caa7261de16fab00cc
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-30 11:02:37 +0200

    Update second_task.c

--- second_task.c
+++ second_task.c
@@ -1,5 +1,6 @@
 /* Task description:
-
+Write a function that receives the length of an edge of a cube as a parameter. The function should return the volume and the surface area of the cube in two further parameters passed by address. 
+(The return type of the function is void.) Call this function from the main to calculate the volume and the surface area of a cube of 2.7 edge length, then print the results in main!
 
 */
 
